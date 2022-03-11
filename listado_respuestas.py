733c57da65334e804a7d14edd23731dade924764
FernandoCamousseigt <82519981+FernandoCamousseigt@users.noreply.github.com>
2022-03-11 16:15:33 -0300

    Delete listado_respuestas.py

--- listado_respuestas.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractclassmethod
-
-class Listado_Respuestas():
-    def __init__(self, listado_respuestas: int,):
-        self.listado_respuestas = listado_respuestas
-        listado_respuestas = []
-   
